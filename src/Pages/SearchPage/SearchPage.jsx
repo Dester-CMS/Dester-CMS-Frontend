3fac6205de6d4e7a8bd075a635720c17caedebaa
Alken Dester <96910178+Dester-Alken@users.noreply.github.com>
2022-01-16 20:17:46 +0530

    Update SearchPage.jsx

--- src/Pages/SearchPage/SearchPage.jsx
+++ src/Pages/SearchPage/SearchPage.jsx
@@ -1,85 +1,20 @@
 import React, { useState } from 'react'
-import { Col, Container, FormControl, InputGroup, Row } from 'react-bootstrap';
+import { Button, Col, Container, FormControl, InputGroup, Row } from 'react-bootstrap';
 import { ItemDesignOne } from '../../Components';
 import "./style.css"
-// import { useFetchMultiMoviesUrl, useFetchMultiSeriesUrl } from '../../utilities/useFetchSecureUrl';
 
 const SearchPage = ({ movieResult, serieResult, loadingSecureMoviesUrl, loadingSecureSeriesUrl, errorSecureMoviesUrl, errorSecureSeriesUrl }) => {
 
     const [searchInput, setSearchInput] = useState('');
     const [filteredResultsMovies, setFilteredResultsMovies] = useState([]);
     const [filteredResultsSeries, setFilteredResultsSeries] = useState([]);
-
-    // Fetching Data
-
-    // const { 
-    //     dataMainSecureMoviesUrl,
-    //     dataTmdbSecureMoviesUrl,
-    //     loadingSecureMoviesUrl,
-    //     errorSecureMoviesUrl
-    // } = useFetchMultiMoviesUrl(process.env.REACT_APP_S_API_URL, process.env.REACT_APP_SECURE_URL);
-    
-    //  const { 
-    //     dataMainSecureSeriesUrl,
-    //     dataTmdbSecureSeriesUrl,
-    //     loadingSecureSeriesUrl,
-    //     errorSecureSeriesUrl
-    // } = useFetchMultiSeriesUrl(process.env.REACT_APP_S_API_URL, process.env.REACT_APP_SECURE_URL);
+    const [genreFilter, setGenreFilter] = useState([])
 
     if (loadingSecureMoviesUrl && loadingSecureSeriesUrl) return <h3 className="color-white p-3">Loading...</h3>
 
     // Error Handleing
     if (errorSecureMoviesUrl) console.log(errorSecureMoviesUrl);
     if (errorSecureSeriesUrl) console.log(errorSecureSeriesUrl);
-
-    // // Variables to store API fetch results
-    // let movieResult = "", serieResult = "";
-
-    // // Combineing main URL data and TMDB data
-    // // Example for Movie movie.custom_name_field = tmdbItem ? tmdbItem.pick_a_value_to_combine
-    // if(dataMainSecureMoviesUrl && dataTmdbSecureMoviesUrl) {
-    //     movieResult = dataMainSecureMoviesUrl.map(movie => {
-
-    //     const tmdbItem = dataTmdbSecureMoviesUrl.find(tmdbMovie => tmdbMovie.id === movie.tmdb_id);
-        
-    //     movie.tmdb_title = tmdbItem ? tmdbItem.title : null;
-    //     movie.tmdb_overview = tmdbItem ? tmdbItem.overview : null;
-    //     movie.tmdb_poster_path = tmdbItem ? tmdbItem.poster_path : null;
-    //     movie.tmdb_backdrop_path = tmdbItem ? tmdbItem.backdrop_path : null;
-    //     movie.tmdb_genres = tmdbItem ? tmdbItem.genres : null;
-    //     movie.tmdb_extra_images = tmdbItem ? tmdbItem.images : null;
-    //     movie.tmdb_release_date = tmdbItem ? tmdbItem.release_date : null;
-    //     movie.tmdb_release_status = tmdbItem ? tmdbItem.status : null;
-    //     movie.tmdb_production_companies = tmdbItem ? tmdbItem.production_companies : null;
-    //     movie.tmdb_video_runtime = tmdbItem ? tmdbItem.runtime : null;
-    //     movie.tmdb_vote_average = tmdbItem ? tmdbItem.vote_average : null;
-        
-    //     return movie;
-
-    //     });
-    // };
-
-    // if(dataMainSecureSeriesUrl && dataTmdbSecureSeriesUrl) {
-    //     serieResult = dataMainSecureSeriesUrl.map(serie => {
-
-    //     const tmdbItem = dataTmdbSecureSeriesUrl.find(tmdbSerie => tmdbSerie.id === serie.tmdb_id);
-        
-    //     serie.tmdb_title = tmdbItem ? tmdbItem.name : null;
-    //     serie.tmdb_overview = tmdbItem ? tmdbItem.overview : null;
-    //     serie.tmdb_poster_path = tmdbItem ? tmdbItem.poster_path : null;
-    //     serie.tmdb_backdrop_path = tmdbItem ? tmdbItem.backdrop_path : null;
-    //     serie.tmdb_genres = tmdbItem ? tmdbItem.genres : null;
-    //     serie.tmdb_extra_images = tmdbItem ? tmdbItem.images : null;
-    //     serie.tmdb_release_date = tmdbItem ? tmdbItem.release_date : null;
-    //     serie.tmdb_release_status = tmdbItem ? tmdbItem.status : null;
-    //     serie.tmdb_production_companies = tmdbItem ? tmdbItem.production_companies : null;
-    //     serie.tmdb_video_runtime = tmdbItem ? tmdbItem.runtime : null;
-    //     serie.tmdb_vote_average = tmdbItem ? tmdbItem.vote_average : null;
-        
-    //     return serie;
-
-    //     });
-    // };
 
     const searchItems = (searchValue) => {
         setSearchInput(searchValue)
@@ -103,6 +38,16 @@ const SearchPage = ({ movieResult, serieResult, loadingSecureMoviesUrl, loadingS
         };
     }
 
+    const searchItemsByGenre = (currentGenre) => {
+        const resultGenre = movieResult.map((movie) => {
+            movieResult.find(genreMovie => movie.tmdb_genres === currentGenre)
+            return genreMovie
+        });
+        setGenreFilter(resultGenre)
+    }
+
+    console.log(genreFilter)
+
     return (
         <main>
             {movieResult && serieResult && (
@@ -117,8 +62,24 @@ const SearchPage = ({ movieResult, serieResult, loadingSecureMoviesUrl, loadingS
                                 onChange={(e) => searchItems(e.target.value)}
                             />
                         </InputGroup>
+                        <div>
+                            <Button>All</Button>{' '}
+                            <Button tmdb_genre_id="Action" onClick={() => searchItemsByGenre('Action')}>Action</Button>{' '}
+                            <Button tmdb_genre_id="Animation" onClick={() => searchItemsByGenre('Animation')}>Animation</Button>{' '}
+                            <Button tmdb_genre_id="Drama" onClick={() => searchItemsByGenre('Drama')}>Drama</Button>
+                        </div>
                     </div>
                     <Row className="p-2">
+                    <h4>Movies Found</h4>
+                        {genreFilter.map((item) => {
+                            return (
+                                <Col key={item.id} className="p-lg-4 p-sm-3" xs={6} sm={4} md={3} lg={3} xl={2}>
+                                    <ItemDesignOne type="movie" item={item}/>
+                                </Col>
+                            )
+                        })}
+                    </Row>
+                    {/* <Row className="p-2">
                     <h4>Movies Found</h4>
                         {searchInput.length > 1 ? (
                             filteredResultsMovies.map((item) => {
@@ -157,7 +118,7 @@ const SearchPage = ({ movieResult, serieResult, loadingSecureMoviesUrl, loadingS
                                 )
                             })
                         )}
-                    </Row>
+                    </Row> */}
                 </Container>
             )}
         </main>
